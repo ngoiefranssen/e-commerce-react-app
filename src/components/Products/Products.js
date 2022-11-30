a6db9ee7bb68d93fd3a288f4b77082b1ed3f9155
Franssen <ngoiefranssen04@gmail.com>
2022-11-30 16:19:05 +0200

    Wip

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Products = () => {
 
+    const [data, setData] = useState([]);
+    const [filter, setFilter] = useState(data);
+    const [loading, setLoading] = useState(false);
+
+    let componentMoundted = true;
+
+    useEffect(() =>{
+
+    }, [])
 
   return (
     <div>
