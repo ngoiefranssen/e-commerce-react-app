d90c10670c5c8cae21f9509b3bab51e4717b9013
Franssen <ngoiefranssen04@gmail.com>
2022-11-30 20:58:02 +0200

    Wip

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -50,15 +50,16 @@ const Products = () => {
                 {filter.map((product) =>{
                     return(
                         <>
-                            <div className='col-md-3'>
+                            <div className='col-md-3 mb-4'>
                                 <div class="card h-100 text-center p-4" key={product.id}>
-                                    <img src={ product.image } class="card-img-top" alt={product.title} />
+                                    <img src={ product.image } class="card-img-top"
+                                         alt={product.title} height="250px"/>
                                     <div class="card-body">
-                                        <h5 class="card-title" height="250px">
-                                            { product.title.substring(0, 12) }
+                                        <h5 class="card-title mb-0">
+                                            { product.title.substring(0, 12) }..
                                         </h5>
-                                        <p class="card-text">${ product.price }</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        <p class="card-text lead fw-bold">${ product.price }</p>
+                                        <a href="#" class="btn btn-outline-primary">By now</a>
                                     </div>
                                 </div>
                             </div>
