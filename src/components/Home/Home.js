c8ac0b67566b9c4352b253e48637294ea70811b9
Franssen <ngoiefranssen04@gmail.com>
2022-11-30 16:08:04 +0200

    Wip

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="card text-bg-dark border-0">
             <img src="/assets/star.jpg" class="card-img" alt="Background"
                  height='550px' />
-            <div class="card-img-overlay">
+            <div class="card-img-overlay d-flex flex-column justify-content-center">
               <div className='container'>
               <h5 className="card-title display-3 fw-bolder mb-0">Card title</h5>
                 <p className="card-text">
@@ -15,7 +15,7 @@ const Home = () => {
                     lead-in to additional content. This content is a little bit 
                     longer.
                 </p>
-                <p className="card-text lea"><small>CHECK OUT THE TRENDS</small></p>
+                <p className="card-text lead fs-2"><small>CHECK OUT THE TRENDS</small></p>
               </div>
             </div>
         </div>
