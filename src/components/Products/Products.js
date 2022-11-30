87e6c4cb83704d757d0177c647a892863290d5bd
Franssen <ngoiefranssen04@gmail.com>
2022-11-30 16:47:57 +0200

    Wip

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -34,8 +34,10 @@ const Products = () => {
     <div>
       <div className='container my-5 py-5'>
         <div className='row'>
-            <div className='col-12'>
-                <h1>Latest Products</h1>
+            <div className='col-12 mb-5'>
+                <h1 className='display-6 fw-bolder text-center'>
+                    Latest Products
+                </h1>
             </div>
         </div>
       </div>
