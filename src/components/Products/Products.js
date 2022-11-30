1d8180a7ab672225acd6d321790d8d1ff0b6c629
Franssen <ngoiefranssen04@gmail.com>
2022-11-30 16:25:14 +0200

    Wip

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -11,9 +11,16 @@ const Products = () => {
     useEffect(() =>{
 
         const getProducts = async () =>{
+            setLoading(true);
+            const responseData = fetch('https://fakestoreapi.com/products');
 
+            if(componentMoundted)
+            {
+                setData(await responseData.clone().json());
+            }
         };
     }, []);
+
 
   return (
     <div>
