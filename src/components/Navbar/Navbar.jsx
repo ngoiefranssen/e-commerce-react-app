7ad86f1573d815c0fac78d113c6e09dc1315b644
Franssen <ngoiefranssen04@gmail.com>
2022-11-30 14:55:04 +0200

    Wip

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -33,10 +33,7 @@ import React from 'react'
                             <a class="nav-link disabled">Disabled</a>
                         </li>
                     </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                    
                 </div>
             </div>
         </nav>
