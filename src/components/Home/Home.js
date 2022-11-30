cbaedb5de2afc88b1b9ea287e7ff650c7d23860e
Franssen <ngoiefranssen04@gmail.com>
2022-11-30 16:03:57 +0200

    wIP

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,16 +4,19 @@ const Home = () => {
 
   return(
     <div className="hero">
-        <div className="card text-bg-dark">
-            <img src="/assets/star.jpg" class="card-img" alt="Background" />
+        <div className="card text-bg-dark border-0">
+            <img src="/assets/star.jpg" class="card-img" alt="Background"
+                 height='550px' />
             <div class="card-img-overlay">
-                <h5 className="card-title">Card title</h5>
+              <div className='container'>
+              <h5 className="card-title display-3 fw-bolder mb-0">Card title</h5>
                 <p className="card-text">
                     This is a wider card with supporting text below as a natural 
                     lead-in to additional content. This content is a little bit 
                     longer.
                 </p>
-                <p className="card-text"><small>Last updated 3 mins ago</small></p>
+                <p className="card-text lea"><small>CHECK OUT THE TRENDS</small></p>
+              </div>
             </div>
         </div>
     </div>
