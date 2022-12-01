af7ff05c9af2f2856e324702b03c1e15d43ad579
Franssen <ngoiefranssen04@gmail.com>
2022-12-01 09:54:56 +0200

    Wip4

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-light bg-white py-4 shadow-sm">
         <div className="container">
-          <a className="navbar-brand fw-bold fs-4" href="#">
+          <NavLink className="navbar-brand fw-bold fs-4" to="/">
             Collection
-          </a>
+          </NavLink>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,36 +23,36 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <NavLink className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <NavLink className="nav-link" to="/products">
                   Products
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <NavLink className="nav-link" to="/about">
                   About
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <NavLink className="nav-link" to="/contact">
                   Contact
-                </a>
+                </NavLink>
               </li>
             </ul>
             <div className="buttons">
-              <a className="btn btn-outline-dark">
-                <i className="fa fa-sign-in me-1"></i> Sing-In
-              </a>
-              <a className="btn btn-outline-success ms-2">
-                <i className="fa fa-user-plus me-2 ms"></i> Sing-Up
-              </a>
-              <a className="btn btn-outline-dark ms-2">
-                <i className="fa fa-shopping-cart me-1"></i> Card(0)
-              </a>
+              <NavLink className="btn btn-outline-dark">
+                <i className="fa fa-sign-in me-1" to='/login'></i> Login
+              </NavLink>
+              <NavLink className="btn btn-outline-success ms-2">
+                <i className="fa fa-user-plus me-2 ms" to='/sing_up'></i> Sing-Up
+              </NavLink>
+              <NavLink className="btn btn-outline-dark ms-2">
+                <i className="fa fa-shopping-cart me-1" to='/cart'></i> Card(0)
+              </NavLink>
             </div>
           </div>
         </div>
---
+++ src/components/Product/Product.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const Product = () =>{
+
+    const {id} = useParams();
+    const [product, setProduct] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        
+        const getProduct = async () => {
+            
+            const responseDataProduct = await fetch(`https://fakestoreapi.com/products/${id}`);
+            setProduct(await responseDataProduct.json());
+            setLoading( false);
+        }
+        getProduct();
+    },[input]);
+    
+    return(
+        <>
+        </>
+    );
+}; 
+export default Product;
