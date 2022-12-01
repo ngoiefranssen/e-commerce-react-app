d5c8b01cc12288943d5ec91580a4235af3084899
Franssen <ngoiefranssen04@gmail.com>
2022-12-01 09:27:38 +0200

    Wip

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Products from './components/Products/Products';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div>
-        <Navbar />
-        <Home />
-    </div>
+    <>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/products' element={<Products />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
